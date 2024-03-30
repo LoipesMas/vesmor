a38889931ce9c40205af1fd117e90d4fc4097161
LoipesMas <46327403+LoipesMas@users.noreply.github.com>
2024-03-30 15:28:00 +0100

    improve reloading and some styling

--- vvc/js/index.js
+++ vvc/js/index.js
@@ -2,6 +2,7 @@ import("../pkg/index.js").then(module =>{
     //TODO: pass source code here somehow
    module.main_web();
    window.update_source_code = module.update_source_code;
+   window.full_reload();
 });
 
 function full_reload() {
@@ -18,3 +19,22 @@ function hot_reload() {
 }
 window.full_reload = full_reload;
 window.hot_reload = hot_reload;
+
+
+// load default source code
+function reset_code() {
+    fetch("game.vvc").then(resp => resp.text()).then(text => {
+        const input = document.getElementById("sourceText")
+        input.value = text;
+        full_reload();
+    })
+}
+window.reset_code = reset_code;
+
+fetch("game.vvc").then(resp => resp.text()).then(text => {
+    const input = document.getElementById("sourceText")
+    if (input.value === "") {
+        input.value = text;
+    }
+})
+
