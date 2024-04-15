e1ace444ea6d295947300f3f1b7af772ea9bbaf3
LoipesMas <46327403+LoipesMas@users.noreply.github.com>
2024-04-15 19:29:44 +0200

    chore(web): prettier all the files

--- vvc/js/game_loader.ts
+++ vvc/js/game_loader.ts
@@ -1,35 +1,34 @@
-
 type Game = {
-    main_web(): Promise<void>;
-    update_source_code(code: string, hot_reload: boolean): Promise<string>;
-    check_code(code: string): Promise<string>;
+  main_web(): Promise<void>;
+  update_source_code(code: string, hot_reload: boolean): Promise<string>;
+  check_code(code: string): Promise<string>;
 };
 let game: Game | null = null;
 
-const error_msg = "no game :("
+const error_msg = "no game :(";
 export async function full_reload(source: string): Promise<string> {
-    if (game) {
+  if (game) {
     return game.update_source_code(source, false);
-    } else {
-        return error_msg
-    }
+  } else {
+    return error_msg;
+  }
 }
 export async function hot_reload(source: string): Promise<string> {
-    if (game) {
+  if (game) {
     return game.update_source_code(source, true);
-    } else {
-        return error_msg
-    }
+  } else {
+    return error_msg;
+  }
 }
 export async function check_code(source: string): Promise<string> {
-    if (game) {
+  if (game) {
     return game.check_code(source);
-    } else {
-        return error_msg
-    }
+  } else {
+    return error_msg;
+  }
 }
 
 export async function init() {
-    game = await import("../pkg/index.js");
-    game.main_web()
+  game = await import("../pkg/index.js");
+  game.main_web();
 }
--- vvc/js/index.js
+++ vvc/js/index.js
@@ -1,33 +1,48 @@
-import { get_code, set_output, reset_code, init as init_editor } from "./editor.ts";
-import { full_reload, hot_reload, check_code, init as init_game } from "./game_loader.ts";
+import {
+  get_code,
+  set_output,
+  reset_code,
+  init as init_editor,
+} from "./editor.ts";
+import {
+  full_reload,
+  hot_reload,
+  check_code,
+  init as init_game,
+} from "./game_loader.ts";
 
 import "./style.scss";
 
 window.full_reload = () => {
-    let source = get_code();
-    if (source != undefined && source != null) {
-        full_reload(source).then(set_output);
-    }
+  let source = get_code();
+  if (source != undefined && source != null) {
+    full_reload(source).then(set_output);
+  }
 };
 window.hot_reload = () => {
-    let source = get_code();
-    if (source !== undefined) {
-        hot_reload(source).then(set_output);
-    }
+  let source = get_code();
+  if (source !== undefined) {
+    hot_reload(source).then(set_output);
+  }
 };
 window.reset_code = () => {
-    reset_code("game.vvc").then(window.full_reload);
+  reset_code("game.vvc").then(window.full_reload);
 };
 
 function check_source_code() {
-    let source = get_code();
-    if (source) {
-        check_code(source).then((result) => {
-            set_output(result);
-        });
-    }
+  let source = get_code();
+  if (source) {
+    check_code(source).then((result) => {
+      set_output(result);
+    });
+  }
 }
 window.check_source_code = check_source_code;
 
-setTimeout(() => {init_game()},0)
-setTimeout(() => {init_editor(); reset_code("game.vvc")},0)
+setTimeout(() => {
+  init_game();
+}, 0);
+setTimeout(() => {
+  init_editor();
+  reset_code("game.vvc");
+}, 0);
