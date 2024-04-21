63313f6b3a37b9de65b121706070ffb2cf72134b
LoipesMas <46327403+LoipesMas@users.noreply.github.com>
2024-04-21 21:33:23 +0200

    chore(web): format

--- vvc/js/editor.ts
+++ vvc/js/editor.ts
@@ -7,81 +7,81 @@ import { tags } from "@lezer/highlight";
 import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
 
 const highlights = HighlightStyle.define([
-  { tag: tags.typeName, color: "#c96abc" },
-  { tag: tags.operatorKeyword, color: "#71bc8c" },
-  { tag: tags.punctuation, color: "#9b9992" },
-  { tag: tags.integer, color: "#4e92b7" },
+    { tag: tags.typeName, color: "#c96abc" },
+    { tag: tags.operatorKeyword, color: "#71bc8c" },
+    { tag: tags.punctuation, color: "#9b9992" },
+    { tag: tags.integer, color: "#4e92b7" },
 ]);
 
 const keymaps = [
-  {
-    key: "Ctrl-r",
-    run: (ev: EditorView) => {
-      let selection = ev.state.selection.main;
-      let current_pos = selection.from;
-      console.log(current_pos);
-      let t = ev.state.update(
-        {
-          changes: { from: current_pos, insert: "<  >" },
-          sequential: true,
+    {
+        key: "Ctrl-r",
+        run: (ev: EditorView) => {
+            let selection = ev.state.selection.main;
+            let current_pos = selection.from;
+            console.log(current_pos);
+            let t = ev.state.update(
+                {
+                    changes: { from: current_pos, insert: "<  >" },
+                    sequential: true,
+                },
+                {
+                    selection: {
+                        anchor: current_pos + 2,
+                        head: current_pos + 2,
+                    },
+                    sequential: true,
+                }
+            );
+            ev.update([t]);
+            return true;
         },
-        {
-          selection: {
-            anchor: current_pos + 2,
-            head: current_pos + 2,
-          },
-          sequential: true,
-        },
-      );
-      ev.update([t]);
-      return true;
+        preventDefault: true,
     },
-    preventDefault: true,
-  },
 ];
 
 const theme = EditorView.theme(
-  {
-    "&": {
-      width: "70vh",
-      height: "75vh !important",
-      backgroundColor: "#001524",
-      fontSize: "larger",
-      color: "#ff7d00",
-      fontWeight: "bold",
-      border: "1px solid #ff7d00",
-      padding: "0.5rem",
+    {
+        "&": {
+            width: "70vh",
+            height: "75vh",
+            backgroundColor: "#001524",
+            fontSize: "larger",
+            color: "#ff7d00",
+            fontWeight: "bold",
+            border: "1px solid #ff7d00",
+            padding: "0.5rem",
+        },
+        ".cm-content": {
+            caretColor: "#ff7d00",
+        },
+        "&.cm-focused .cm-cursor": {
+            borderLeftColor: "#0e9",
+        },
+        "&.cm-focused .cm-selectionBackground, ::selection": {
+            backgroundColor: "#074",
+        },
+        ".cm-gutters": {
+            backgroundColor: "#045",
+            color: "#ddd",
+            border: "none",
+        },
     },
-    ".cm-content": {
-      caretColor: "#ff7d00",
-    },
-    "&.cm-focused .cm-cursor": {
-      borderLeftColor: "#0e9",
-    },
-    "&.cm-focused .cm-selectionBackground, ::selection": {
-      backgroundColor: "#074",
-    },
-    ".cm-gutters": {
-      backgroundColor: "#045",
-      color: "#ddd",
-      border: "none",
-    },
-  },
-  { dark: true },
+    { dark: true }
 );
 
 function state_from_code(code: string): EditorState {
-  return EditorState.create({
-    doc: code,
-    extensions: [
-      basicSetup,
-      // keymap.of(defaultKeymap),
-      vvcl(),
-      theme,
-      syntaxHighlighting(highlights),
-      keymap.of(keymaps),
-    ],
-  });
+    return EditorState.create({
+        doc: code,
+        extensions: [
+            basicSetup,
+            // keymap.of(defaultKeymap),
+            vvcl(),
+            theme,
+            syntaxHighlighting(highlights),
+            keymap.of(keymaps),
+        ],
+    });
 }
 
 let startState = state_from_code("Loading...");
@@ -91,34 +91,34 @@ let output_el: HTMLElement | null = null;
 export let view: EditorView | null = null;
 
 export function init() {
-  let el = document.getElementById("codemirror-holder");
-  output_el = document.getElementById("output");
-  if (el !== null) {
-    view = new EditorView({
-      state: startState,
-      parent: el,
-    });
-  }
-  return view;
+    let el = document.getElementById("codemirror-holder");
+    output_el = document.getElementById("output");
+    if (el !== null) {
+        view = new EditorView({
+            state: startState,
+            parent: el,
+        });
+    }
+    return view;
 }
 
 export function get_code(): string | undefined {
-  return view?.state.doc.toString();
+    return view?.state.doc.toString();
 }
 export function set_code(code: string) {
-  if (view) {
-    view.setState(state_from_code(code));
-  }
+    if (view) {
+        view.setState(state_from_code(code));
+    }
 }
 
 export function set_output(output: string) {
-  if (output_el) {
-    output_el.innerText = output;
-  }
+    if (output_el) {
+        output_el.innerText = output;
+    }
 }
 
 export async function reset_code(source: string): Promise<void> {
-  return fetch(source)
-    .then((resp) => resp.text())
-    .then(set_code);
+    return fetch(source)
+        .then((resp) => resp.text())
+        .then(set_code);
 }
